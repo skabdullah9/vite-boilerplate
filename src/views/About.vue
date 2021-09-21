4d5c6b8fbe792e5b9d04cdb4e0fea014324398d9
skabdullah9 <skabdullah9941@gmail.com>
2021-09-21 21:54:50 +0530

    Initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <h1>{{ name }}</h1>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+
+const name = computed(() => {
+    return store.state.user.name
+})
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>name in store is {{ name }}</h1>
+        <input
+            type="text"
+            placeholder="new name"
+            v-model="newName"
+            name=""
+            id=""
+            class="border"
+        />
+        <button @click="saveName" class="bg-indigo-500 text-white">
+            Change name
+        </button>
+    </div>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const name = computed(() => {
+    return store.state.user.name
+})
+
+const newName = ref('')
+const saveName = () => {
+    store.dispatch('setName', newName.value)
+    newName.value = ''
+    router.push('/about')
+}
+</script>
+
+<style></style>
